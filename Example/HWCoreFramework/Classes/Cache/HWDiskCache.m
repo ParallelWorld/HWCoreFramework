ae6154c87b0ffac86a96ac87b16ff9fd259100c7
parallelWorld <654269765@qq.com>
2016-07-28 11:14:00 +0800

    增加diskCache的唯一性解决方案

--- Example/HWCoreFramework/Classes/Cache/HWDiskCache.m
+++ Example/HWCoreFramework/Classes/Cache/HWDiskCache.m
@@ -7,9 +7,68 @@
 //
 
 #import "HWDiskCache.h"
+#import <CommonCrypto/CommonCrypto.h>
+
 
 #define LOCK() dispatch_semaphore_wait(self->_lock, DISPATCH_TIME_FOREVER)
 #define UNLOCK() dispatch_semaphore_signal(self->_lock)
+
+
+/// Free disk space in bytes.
+static int64_t _HWDiskSpaceFree() {
+    NSError *error = nil;
+    NSDictionary *attrs = [[NSFileManager defaultManager] attributesOfFileSystemForPath:NSHomeDirectory() error:&error];
+    if (error) return -1;
+    int64_t space = [attrs[NSFileSystemFreeSize] longLongValue];
+    if (space < 0) space = -1;
+    return space;
+}
+
+/// String's md5 hash.
+static NSString *_HWNSStringMD5(NSString *string) {
+    if (!string) return nil;
+    NSData *data = [string dataUsingEncoding:NSUTF8StringEncoding];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(data.bytes, (CC_LONG)data.length, result);
+    return [NSString stringWithFormat:
+            @"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+            result[0],  result[1],  result[2],  result[3],
+            result[4],  result[5],  result[6],  result[7],
+            result[8],  result[9],  result[10], result[11],
+            result[12], result[13], result[14], result[15]
+            ];
+}
+
+/// Weak reference for all instances
+static NSMapTable *_globalInstances;
+/// Keep disk cache to be unique.
+static dispatch_semaphore_t _globalInstancesLock;
+
+static void _HWDiskCacheInitGlobal() {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _globalInstancesLock = dispatch_semaphore_create(1);
+        _globalInstances = [[NSMapTable alloc] initWithKeyOptions:NSPointerFunctionsStrongMemory valueOptions:NSPointerFunctionsWeakMemory capacity:0];
+    });
+}
+
+static HWDiskCache *_HWDiskCacheGetGlobal(NSString *path) {
+    if (path.length == 0) return nil;
+    _HWDiskCacheInitGlobal();
+    dispatch_semaphore_wait(_globalInstancesLock, DISPATCH_TIME_FOREVER);
+    id cache = [_globalInstances objectForKey:path];
+    dispatch_semaphore_signal(_globalInstancesLock);
+    return cache;
+}
+
+static void _HWDiskCacheSetGlobal(HWDiskCache *cache) {
+    if (cache.path.length == 0) return;
+    _HWDiskCacheInitGlobal();
+    dispatch_semaphore_wait(_globalInstancesLock, DISPATCH_TIME_FOREVER);
+    [_globalInstances setObject:cache forKey:cache.path];
+    dispatch_semaphore_signal(_globalInstancesLock);
+}
+
 
 @implementation HWDiskCache {
     dispatch_semaphore_t _lock;
@@ -22,13 +81,18 @@
 
 - (instancetype)initWithPath:(NSString *)path {
     self = [super init];
-    if (self) {
-        _path = path;
-        _lock = dispatch_semaphore_create(1);
-        _queue = dispatch_queue_create("com.parallelworld.cache.disk", DISPATCH_QUEUE_CONCURRENT);
-        [self p_createCacheDirectory];
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(p_appWillTerminate) name:UIApplicationWillTerminateNotification object:nil];
-    }
+    if (!self) return nil;
+    
+    HWDiskCache *globalCache = _HWDiskCacheGetGlobal(path);
+    if (globalCache) return globalCache;
+    
+    _path = path;
+    _lock = dispatch_semaphore_create(1);
+    _queue = dispatch_queue_create("com.parallelworld.cache.disk", DISPATCH_QUEUE_CONCURRENT);
+    [self p_createCacheDirectory];
+    _HWDiskCacheSetGlobal(self);
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(p_appWillTerminate) name:UIApplicationWillTerminateNotification object:nil];
     return self;
 }
 
